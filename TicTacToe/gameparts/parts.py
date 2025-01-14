b0b4802aaab36c1f21eafc464e440ebeaf8f6b74
Тимофеев Илья <timofeev_ilya@mail.ru>
2025-01-14 22:40:48 +0300

    TTT2

--- TicTacToe/gameparts/parts.py
+++ TicTacToe/gameparts/parts.py
@@ -1,4 +1,7 @@
+# gameparts/parts.py
 class Board():
+    """Класс для игровых задач"""
+
     field_size = 3
 
     def __init__(self):
@@ -20,3 +23,24 @@ class Board():
             'Объект игрового поля размером '
             f'{self.field_size}x{self.field_size}'
         )
+
+    def is_board_full(self):
+        for i in range(self.field_sizefiel):
+            for j in range(self.field_size):
+                if self.board[i][j] == ' ':
+                    return False
+        return True
+
+    def check_win(self, player):
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+        if (
+            self.board[0][0] == self.board[1][1] == self.board[2][2] == player
+            or
+            self.board[0][2] == self.board[1][1] == self.board[2][0] == player
+        ):
+            return True
+
+        return False
